895ba94b9fbe9b0724739963296992e8ba84211d
ngelecheidk <ngelecheidk@gmail.com>
2025-06-25 05:41:47 +0300

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+"""
+
+for i=0 to 100 in steps of 100 do
+    if i % 3==0 and 1 % 5 == 0 do
+        print fizzbuzz
+    else if i % 3 === 0:
+        print fizz
+    else if  i % 5 ===0 ;
+        print buz
+    else
+        i
+    
+"""
+
+for i in range(20):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
